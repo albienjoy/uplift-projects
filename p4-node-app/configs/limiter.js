8200f3f46ddaeb6218f745fc1845efcd87661dff
Albien <albien.joy@gmail.com>
2026-03-15 22:14:18 +0800

    fix: stopped tracking irrelevant folders

--- p4-node-app/configs/limiter.js
+++
@@ -1,8 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-const limiterConfig = rateLimit({
-  windowMs: 1000 * 60 * 20,
-  max: 100,
-  message: "Too many request from this IP, please try again later!",
-});
-export default limiterConfig;
